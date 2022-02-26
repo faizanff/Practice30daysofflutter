3c593f2d5ba1e14035d2816abbb9ed1ef149b064
faizan ahmed <faizanmalikahmed@gmail.com>
2022-02-26 21:58:23 +0500

    day04

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -4,15 +4,15 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        // ignore: prefer_const_constructors
-        child: Text(
-          "login page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.deepOrange,
-            fontWeight: FontWeight.bold,
-          ),
+      child: Column(children: [
+        Image.asset("assets/images/login_image.png",
+        fit: BoxFit.cover,
+        ),
+
+        Text("Welcome",style: TextStyle(
+          
+
+        ),
         ),
       ),
     );
